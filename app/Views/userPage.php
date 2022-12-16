91f0a67d1df345533f0a02186ac2d5e19b875263
luckyman0333 <82233343+luckyman0333@users.noreply.github.com>
2022-12-16 21:51:03 +0100

    Modyfikacja 2 stron
    Modyfikacja strony singlePost.php(dodanie komentów),
    i strony userPage.php(stwożenie od zera)

--- app/Views/userPage.php
+++ app/Views/userPage.php
@@ -11,12 +11,78 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 ?>
-<div class="container-fluid">
-    <h1>Strona użytkownika</h1>
-    <h3>Email: <?= $_SESSION['userEmail'] ?></h3>
-    <h3>Nazwa uzytkownika: <?= $_SESSION['userName'] ?></h3>
-    <a href="/User/changePassword">Przejdz do zmiany hasla</a><br>
-    <a href="/User/deleteUser/<?= $_SESSION['userID'] ?>">Usuń hasło</a>
+<div class="container-fluid py-5">
+  <div class="w-75 h-75">
+  <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10"> <!-- zmienić ogólną szerokość okienka można tutaj "col-xl-10"-->
+          <div class="card" style="border-radius: 1rem;">
+            <div class="row g-0">
+
+
+
+              
+              <div class="col-md-12 d-flex align-items-center"><!--blok danych osobistych mam 4 columny do przycisku usun-->
+                <div class="card-body p-4 p-lg-5 text-black">
+                    <div class="d-flex align-items-center mb-3 pb-1">
+                      <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                      <span class="h1 fw-bold mb-0">Strona użytkownika</span>
+                    </div>
+  
+                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Dane użytkownika</h5>
+  
+                    <div class="form-outline mb-4">
+                      <h3>Email: <?= $_SESSION['userEmail'] ?></h3>
+                    </div>
+  
+                    <div class="form-outline mb-4">
+                      <h3>Nazwa uzytkownika: <?= $_SESSION['userName'] ?></h3>
+                    </div>
+
+                    <div class="form-outline mb-4">
+                      <a class="btn btn-dark btn-lg btn-block" href="/User/changePassword">Przejdz do zmiany hasla</a>
+                    </div>
+
+                    <div class="form-outline mb-4">
+                      <a class="btn btn-dark btn-lg btn-block" href="/User/deleteUser/<?= $_SESSION['userID'] ?>">Usuń hasło</a>
+                    </div>
+
+                    <div class="form-outline mb-4 text-end">
+                      <a class="btn btn-outline-danger btn-lg btn-block" data-bs-toggle="modal" data-bs-target="#myModal">Usuń konto</a>
+                    </div>
+
+                    <!-- modal window====================================-->
+                    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="modatl-title" aria-hidden="true">
+                      <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalCenterTitle">Usuwanie konta</h5>
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body">
+                            Czy naprawde chcesz usunąć konto?
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <a type="button" class="btn btn-outline-danger" href="/User/deleteUser/<?= $_SESSION['userID'] ?>">Usuń konto</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- modal window====================================-->
+                </div>
+              </div>
+
+
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
 </div>
 
 <?= $this->endSection() ?>
